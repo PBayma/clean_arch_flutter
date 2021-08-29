5f9b1fd40902c6f1ae460db67cc76ba94502ad16
Pedro Bayma <pedro_bayma@outlook.com>
2021-08-29 13:55:28 -0300

    Creating the NumberTriviaData, NumberTrivia JSON serialization on NumberTriviaData and its tests

---
+++ lib/features/number_trivia/data/models/number_trivia_data.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:clean_flutter_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  const NumberTriviaModel({required int number, required String text})
+      : super(number: number, text: text);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
+
+  factory NumberTriviaModel.fromMap(Map<String, dynamic> map) {
+    return NumberTriviaModel(
+      text: map['text'],
+      number: (map['number'] as num).toInt(),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory NumberTriviaModel.fromJson(String source) =>
+      NumberTriviaModel.fromMap(json.decode(source));
+}
---
+++ test/features/number_trivia/data/models/number_trivia_data_test.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:clean_flutter_tdd/features/number_trivia/data/models/number_trivia_data.dart';
+import 'package:clean_flutter_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  const tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'test text');
+  test('should be a subclass of NumberTrivia entity', () async {
+    expect(tNumberTriviaModel, isA<NumberTrivia>());
+    expect(tNumberTriviaModel.number, equals(1));
+    expect(tNumberTriviaModel.text, equals('test text'));
+  });
+  group('fromJson ', () {
+    test('should return  a valid number when the JSON number is a integer', () {
+      final result = NumberTriviaModel.fromJson(fixture('trivia.json'));
+
+      expect(result, equals(tNumberTriviaModel));
+    });
+
+    test(
+        'should return  a valid number when the JSON number is regarded as double',
+        () {
+      final result = NumberTriviaModel.fromJson(fixture('trivia_double.json'));
+
+      expect(result, equals(tNumberTriviaModel));
+    });
+  });
+
+  group('toJson ', () {
+    test('should return  a valid number when the JSON number is a integer', () {
+      final result = tNumberTriviaModel.toJson();
+
+      expect(result, equals('{"text":"test text","number":1}'));
+    });
+  });
+}
